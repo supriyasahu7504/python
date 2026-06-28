629a5b048b9bf7697510b81217bbf1f7ce63220c
supriyasahu7504 <supriyasahu7504@gmail.com>
2026-06-28 16:22:49 +0000

    day2 working

---
+++ PythonWeek1/MiniProject2.py
@@ -0,0 +1,16 @@
+print("Student Information:")
+name = input("Enter Your Name:")
+age = int(input("Enter Your Age:"))
+college = input("Enter Your College:")
+branch = input("Enter Your Branch:")
+cgpa = float(input("Enter Your Cgpa:"))
+city = input("Enter Your City:")
+
+
+print("Name:", name)
+print("Age:", age)
+print("College:", college)
+print("Branch:", branch)
+print("Cgpa:", cgpa)
+print("City:", city)
+
---
+++ PythonWeek1/TypeConversion.py
@@ -0,0 +1,15 @@
+# #Integer Example
+age = int(input("Enter your age: "))
+
+print("Next year you will be:", age + 1)
+
+# float Example
+cgpa = float(input("Enter your cgpa"))
+print("cgpa :",cgpa)
+
+# String example
+salary = str(input("enter your salary"))
+print("salary:",salary)
+
+
+
