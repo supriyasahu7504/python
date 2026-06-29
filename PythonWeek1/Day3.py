77ccb89cfd5f269b99742e9d798b08b6af8b5831
supriyasahu7504 <supriyasahu7504@gmail.com>
2026-06-29 13:28:55 +0000

    day3learing

---
+++ PythonWeek1/Day3.py
@@ -0,0 +1,47 @@
+# Topic 1: Arithmetic Operators
+a = 10
+b = 3
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Remainder:", a % b)
+print("Power:", a ** b)
+
+
+# Topic 2: Comparison Operators
+a = 20
+b = 10
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+
+# Topic 3: Assignment Operators
+x = 10
+
+x += 5
+print(x)   # 15
+
+x -= 2
+print(x)   # 13
+
+x *= 3
+print(x)   # 39
+
+x /= 3
+print(x)   # 13.0
+
+
+# Topic 4: Operator Precedence
+result = 10 + 2 * 5
+print(result)
+
+result = (10 + 2) * 5
+print(result)
